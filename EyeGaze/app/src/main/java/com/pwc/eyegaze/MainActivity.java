68f5940525f663193248a27232274c28727cd6b1
Kalaiarasan Somasundaram <u6555407@anu.edu.au>
2020-03-09 01:16:57 +1100

    EyeGaze:Fiddling with Face API

--- EyeGaze/app/src/main/java/com/pwc/eyegaze/MainActivity.java
+++ EyeGaze/app/src/main/java/com/pwc/eyegaze/MainActivity.java
@@ -2,15 +2,23 @@ package com.pwc.eyegaze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
 import com.pwc.eyegaze.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-private ActivityMainBinding binding;
-
+    private ActivityMainBinding binding;
+    private CameraSource cameraSource;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,24 +26,42 @@ private ActivityMainBinding binding;
         View view =binding.getRoot();
         setContentView(view);
 
-binding.yesTextView.setOnTouchListener(new View.OnTouchListener() {
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        scaleNormalOrUp((View) view.getParent(),motionEvent);
-           return false;
-    }
+        binding.yesTextView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                scaleNormalOrUp((View) view.getParent(),motionEvent);
+                return false;
+            }
 
-});
+        });
 
 
-binding.noTextView.setOnTouchListener(new View.OnTouchListener() {
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        scaleNormalOrUp((View) view.getParent(),motionEvent);
+        binding.noTextView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                scaleNormalOrUp((View) view.getParent(),motionEvent);
 
-        return false;
-    }
-});
+                return false;
+            }
+        });
+
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+               .setProminentFaceOnly(true)
+                .setMode(FaceDetector.ACCURATE_MODE)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+        cameraSource = new CameraSource.Builder(getApplicationContext(),detector)
+                .setRequestedPreviewSize(640, 480)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(30.0f)
+                .build();
+     
+//        detector.setProcessor(
+//                new LargestFaceFocusingProcessor(
+//                        detector,
+//                        new FaceTracker()));
+        String apiReady=detector.isOperational()?"ready":"not ready";
+        Toast.makeText(getApplicationContext(),"API "+apiReady,Toast.LENGTH_LONG).show();
     }
     public void scaleNormalOrUp(View v, MotionEvent motionEvent){   int motion=motionEvent.getAction();
         if(motion==MotionEvent.ACTION_DOWN||motion==MotionEvent.ACTION_BUTTON_PRESS||motion==MotionEvent.ACTION_DOWN||motion==MotionEvent.ACTION_BUTTON_PRESS||motion==MotionEvent.AXIS_PRESSURE){
