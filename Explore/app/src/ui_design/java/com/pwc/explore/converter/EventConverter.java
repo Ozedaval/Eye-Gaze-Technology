d9beb24b06b57febcd179b3185dc0b8fad8cd3b8
Minju Kang <minju.kang@outlook.com>
2020-04-16 13:29:48 +1000

    adding project space for ui

---
+++ Explore/app/src/ui_design/java/com/pwc/explore/converter/EventConverter.java
@@ -0,0 +1,5 @@
+package com.pwc.explore.converter;
+
+public abstract class EventConverter {
+    /*TODO (Converts appropriate inputs to appropriate events) */
+}
---
+++ Explore/app/src/ui_design/java/com/pwc/explore/face/FaceCursor.java
@@ -0,0 +1,7 @@
+package com.pwc.explore.face;
+
+import com.pwc.explore.Cursor;
+
+public class FaceCursor extends Cursor {
+    /*TODO (Will contain data on which View object the user has selected in a lifecycle conscious way  ) */
+}
