6285c6801da34791a9f55df9bdf9a22f0a393900
Minju Kang <minju.kang@outlook.com>
2020-05-19 23:19:24 +1000

    lagging issue has been fixed

--- Explore/app/src/main/java/com/pwc/explore/eyegaze/sparseflowSelection/EyeGazeEventActivity.java
+++ Explore/app/src/main/java/com/pwc/explore/eyegaze/sparseflowSelection/EyeGazeEventActivity.java
@@ -1,7 +1,6 @@
 package com.pwc.explore.eyegaze.sparseflowSelection;
 
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -16,11 +15,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.internal.VisibilityAwareImageButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.pwc.explore.MainActivity;
 import com.pwc.explore.R;
@@ -52,7 +53,6 @@ public class EyeGazeEventActivity extends AppCompatActivity implements CameraBri
     private CameraBridgeViewBase camera;
     private CoordinatorLayout coordinatorLayout;
     private TextView eyegazeTextView;
-    TextView calibrationCircle;
     private Detect detect;
     private static final String TAG="EyeGazeEventActivity";
     ItemAdapter itemAdapter;
@@ -60,14 +60,12 @@ public class EyeGazeEventActivity extends AppCompatActivity implements CameraBri
     double []leftTop = new double[2]; // left top
     double[] rightBottom = new double[2];
     double[] middle = new double[2];
-    int leftTopCount=0;
-    int rightBottomCount=0;
-    int middleCount=0;
+    boolean calibration = false;
     ArrayList<Double> cb_eyeX_li = new ArrayList<>();
     ArrayList<Double> cb_eyeY_li = new ArrayList<>();
-    boolean doingCalibration=false;
-    int frameCount=0;
     ImageView aimImage;
+    Runnable runnable;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -88,10 +86,8 @@ public class EyeGazeEventActivity extends AppCompatActivity implements CameraBri
         camera.setCameraPermissionGranted();
         camera.disableFpsMeter();
         camera.setCvCameraViewListener(this);
-
-        detect=new Detect();
         aimImage = (ImageView) findViewById(R.id.aim);
-        calibrationCircle = (TextView) findViewById(R.id.calibrationCircle);
+        detect=new Detect();
 
         faceCascade = new CascadeClassifier();
         eyesCascade = new CascadeClassifier();
@@ -137,38 +133,35 @@ public class EyeGazeEventActivity extends AppCompatActivity implements CameraBri
         builder.setTitle("Calibration");
         builder.setCancelable(false);
         builder.setPositiveButton(
-                        "OK",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                doingCalibration = true;
-                                frameCount=0;
-                                aimImage.setVisibility(View.VISIBLE);
-
-
-                            }
-                        });
+                "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        aimImage.setVisibility(View.VISIBLE);
+                    }
+                });
         builder.setNegativeButton(
-                        "NO",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
-                            }
-                        });
+                "NO",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
         AlertDialog alertDialog = builder.create();
 
         // Show the Alert Dialog box
         alertDialog.show();
-//        for(int i = 0 ; i < 10000 ; i++){
-//               cb_eyeX_li.add(detect.cb_eyeX);
-//               cb_eyeY_li.add(detect.cb_eyeY);
-//            }
-//           cb_eyeX_li = filter_outliers(cb_eyeX_li); cb_eyeY_li = filter_outliers(cb_eyeY_li);
-//           // average of X, Y -> Center of the rectangle
-//            detect.averageX = sum(cb_eyeX_li) / cb_eyeX_li.size();
-//            detect.averageY = sum(cb_eyeY_li) / cb_eyeY_li.size();
-//            detect.calibration_status ++;
+        calibration = true;
+        for(int i = 0 ; i < 10000 ; i++){
+            cb_eyeX_li.add(detect.cb_eyeX);
+            cb_eyeY_li.add(detect.cb_eyeY);
+        }
+        cb_eyeX_li = filter_outliers(cb_eyeX_li); cb_eyeY_li = filter_outliers(cb_eyeY_li);
+        // average of X, Y -> Center of the rectangle
+        detect.averageX = sum(cb_eyeX_li) / cb_eyeX_li.size();
+        detect.averageY = sum(cb_eyeY_li) / cb_eyeY_li.size();
+        detect.calibration_status ++;
     }
 
 
@@ -217,42 +210,28 @@ public class EyeGazeEventActivity extends AppCompatActivity implements CameraBri
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-       eyegazeTextView.post(new Runnable() {
-           @Override
-           public void run() {
-               if(doingCalibration){
-                   frameCount++;
-                   if(frameCount>2){
+        eyegazeTextView.post(new Runnable() {
+            @Override
+            public void run() {
+                eyegazeTextView.setText(detect.getDirection()+"");
+                if(detect.calibration_status == 1){
 
-                       cb_eyeX_li.add(detect.cb_eyeX);
-                       cb_eyeY_li.add(detect.cb_eyeY);
-                   }
-                   if(frameCount>22){
-                       cb_eyeX_li = filter_outliers(cb_eyeX_li); cb_eyeY_li = filter_outliers(cb_eyeY_li);
-                       // average of X, Y -> Center of the rectangle
-                       detect.averageX = sum(cb_eyeX_li) / cb_eyeX_li.size();
-                       detect.averageY = sum(cb_eyeY_li) / cb_eyeY_li.size();
-                       detect.calibration_status ++;
-                       frameCount=0;
-                       doingCalibration=false;
-                       aimImage.setVisibility(View.INVISIBLE);
-
-                   }
-               }
-               eyegazeTextView.setText(detect.getDirection()+"");
-               if(detect.calibration_status == 1){
-
-               }
-
-               if(detect.cb_eyeX > detect.averageX){
-                   itemAdapter.select(0); // consider that it's mirror image
-               }else{
-                   itemAdapter.select(1);
-               }
-           }
-       });
-
-
+                }
+                if(detect.cb_eyeX > detect.averageX){
+                    itemAdapter.select(1); // consider that it's mirror image
+                }else{
+                    itemAdapter.select(0);
+                }
+            }
+        });
+        if(calibration){
+        aimImage.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                aimImage.setVisibility(View.INVISIBLE);
+                calibration =false;
+            }
+        },3000);}
         return detect.detect(inputFrame.rgba(),faceCascade,eyesCascade);
     }
 
